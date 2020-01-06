9af2da80443a9d6636dee99648e44608c07ecf7d
957586323 <957586323@qq.com>
2020-01-06 23:09:16 +0800

    add:抓取页面

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/misaki/crawler
 
 go 1.13
 
-require golang.org/x/text v0.3.2 // indirect
+require (
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,51 @@
-package crawler
+package main
 
-import "golang.org/x/text/transform"
-
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
 //https://goproxy.cn
+
+// golang.org/x/text
+// golang.org/x/net/html
 func main() {
+	resp, err := http.Get(
+		"http://www.zhenai.com/zhenghun")
 
+	if err != nil {
+		panic(err)
+	}
 
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	all, err := ioutil.ReadAll(utf8Reader)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+}
+
+func determineEncoding(r io.Reader)  encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
